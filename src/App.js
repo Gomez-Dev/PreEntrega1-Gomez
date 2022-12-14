cb0ac842801b4d9f62b73a1b7593691a2ceaa924
Gomez-Dev <jofergoz92@mail.com>
2022-12-13 21:11:38 -0300

    item-list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ItemListContairner from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <ItemListContainer />
     </div>
     
   );
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ItemListContainer = () => {
+    return (
+        <P>ItemListContainer</P>
+    )
+}
+
+export default ItemListContainer;
